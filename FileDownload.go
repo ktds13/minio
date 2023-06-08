fb8745334e3990fca7aeb3d5a71291e108319dd6
Khin Thida Soe <khintdsoe@myanmareasenet.com>
2023-06-08 15:17:12 +0630

    object copy from minio to local

---
+++ FileDownload.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"io"
+	"log"
+	"os"
+
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+)
+
+func main() {
+	ctx := context.Background()
+	endpoint := "play.min.io"
+	accessKeyID := "Q3AM3UQ867SPQQA43P2F"
+	secretAccessKey := "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG"
+	useSSL := true
+
+	// Initialize minio client object.
+	minioClient, err := minio.New(endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
+		Secure: useSSL,
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	reader, err := minioClient.GetObject(ctx, "golden-oldies-test", "golden-oldies.zip", minio.GetObjectOptions{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer reader.Close()
+
+	localFile, err := os.Create("my-testfile")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer localFile.Close()
+
+	stat, err := reader.Stat()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if _, err := io.CopyN(localFile, reader, stat.Size); err != nil {
+		log.Fatalln(err)
+	}
+}
